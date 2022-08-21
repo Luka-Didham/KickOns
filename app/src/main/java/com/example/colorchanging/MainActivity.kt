de821e32a5d72ddc098122e5c182115a24116775
Luka-Didham <83997298+Luka-Didham@users.noreply.github.com>
2022-08-21 20:14:32 +1200

    Fully implemented main Gameplay UI

--- app/src/main/java/com/example/colorchanging/MainActivity.kt
+++ app/src/main/java/com/example/colorchanging/MainActivity.kt
@@ -1,5 +1,4 @@
 package com.example.colorchanging
-
 import android.graphics.Color.red
 import android.graphics.drawable.Animatable
 import android.graphics.drawable.AnimationDrawable
@@ -19,16 +18,34 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val screenView = findViewById<RelativeLayout>(R.id.layout)
-        var clicked = 0
+        var clicked = 1
         btnScreen.setOnClickListener {
             clicked += 1
-            if (clicked>3) clicked = 1
-            when(clicked){
-                1 -> btnScreen.text = "Pressed 1"
-                2 -> btnScreen.text = "Pressed 2"
-                3 -> btnScreen.text = "Pressed 3"
+            if (clicked>4){
+                clicked = 1
+                img_powerup.visibility = View.INVISIBLE
+                screenView.background = resources.getDrawable(R.drawable.standard,theme)
+                btnScreen.text = "normal mode: Akshay loves men. If you are akshay take a long cold shower"
+            }
+            if(clicked==2) {
+                btnScreen.text = "handicap mode: Akshay loves men. If you are akshay take a long cold shower"
+                screenView.background = resources.getDrawable(R.drawable.handicap, theme)
+                img_handicap.visibility = View.VISIBLE
+            }
+            if(clicked==3) {
+                img_handicap.visibility = View.INVISIBLE
+                btnScreen.text = "law mode: Akshay loves men. If you are akshay take a long cold shower"
+                screenView.background = resources.getDrawable(R.drawable.law, theme)
+                img_law.visibility = View.VISIBLE
+            }
+            if(clicked==4) {
+                img_law.visibility = View.INVISIBLE
+                btnScreen.text = "powerup: Akshay loves men. If you are akshay take a long cold shower"
+                screenView.background = resources.getDrawable(R.drawable.powerup, theme)
+                img_powerup.visibility = View.VISIBLE
+            }
+
             }
         }
 
     }
-}
