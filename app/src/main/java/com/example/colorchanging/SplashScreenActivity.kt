d11666c00740b6d5b6e9828ead4fd14c598d5ccf
Luka-Didham <83997298+Luka-Didham@users.noreply.github.com>
2022-08-21 20:57:24 +1200

    Added splash screen animation

--- app/src/main/java/com/example/colorchanging/SplashScreenActivity.kt
+++ app/src/main/java/com/example/colorchanging/SplashScreenActivity.kt
@@ -1,11 +1,21 @@
 package com.example.colorchanging
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_splash_screen.*
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+        logo.alpha = 0f
+        logo.animate().setDuration(1500).alpha(1f).withEndAction{
+            val app = Intent(this, MainActivity::class.java)
+            startActivity(app)
+            overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out)
+            finish()
+        }
+
     }
 }
