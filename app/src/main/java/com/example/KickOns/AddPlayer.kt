ec3ec3c0c2563eaebac3b3260a83e0b9aaebcda4
Luka-Didham <83997298+Luka-Didham@users.noreply.github.com>
2022-08-26 04:12:35 +1200

    Working but buggy

--- app/src/main/java/com/example/KickOns/AddPlayer.kt
+++ app/src/main/java/com/example/KickOns/AddPlayer.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.add_player.*
 import kotlinx.android.synthetic.main.card_creation.*
@@ -15,25 +16,51 @@ class AddPlayer : AppCompatActivity() {
 
     private var startGame: MainActivity? = null
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.add_player)
-        var players = ArrayList<String>()
+        var players = ArrayList<Button>()
         var editText = textInputEditText
         var MAX_PLAYERS = 30
 
         btnAddPlayer.setOnClickListener{
-            if(players.size<MAX_PLAYERS) {
-                players.add(editText.toString())
-                editText.setText("")
-
+            var text = editText.getText().toString()
+            print(text)
+            if(text == ""){
+                editText.hint = "Please Add Name"
+                }else{
+                if(players.size<MAX_PLAYERS) {
+                    editText.setText("")
+                    val num = players.size+1
+                    val idString = "btnPlayer$num"
+                    val buttonID = resources.getIdentifier(idString, "id", packageName)
+                    players.add(findViewById(buttonID))
+                    var btn = players[players.size-1]
+                    btn.text = text
+                    btn.visibility = View.VISIBLE
+                    btn.setOnClickListener {
+                        if (players.contains(btn)) {
+                            btn.visibility = View.INVISIBLE
+                            btn.setText("")
+                            players.remove(btn)
+                        }
+                    }
+                }else{
+                    editText.hint = "Max 30 players"
+                }
             }
         }
         btnStartFromChoosePlayers.setOnClickListener{
-            val intent = Intent(this, DeckPicker()::class.java)
-            startActivity(intent)
+            if(players.size>1) {
+                val intent = Intent(this, DeckPicker()::class.java)
+                startActivity(intent)
+            }else{
+                editText.hint = "Minimum 1 player"
+            }
         }
+
 
 
     }
