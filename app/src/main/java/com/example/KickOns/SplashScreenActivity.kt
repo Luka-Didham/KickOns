7cb9814fd1e89edbfc4daecc18e0acdd99f98f48
BlueFox99 <akshaymenon552@gmail.com>
2022-08-26 14:15:06 +1200

    Added delete button to deck selection

--- app/src/main/java/com/example/KickOns/SplashScreenActivity.kt
+++ app/src/main/java/com/example/KickOns/SplashScreenActivity.kt
@@ -1,6 +1,7 @@
 package com.example.KickOns
 
 import android.content.Intent
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_splash_screen.*
