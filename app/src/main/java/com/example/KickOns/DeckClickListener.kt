207261ea7b3c13e40c213d96102fe8c6af703f4b
BlueFox99 <akshaymenon552@gmail.com>
2022-10-03 11:20:41 +1300

    Changed help screen so whole screen is now scrollable

--- app/src/main/java/com/example/KickOns/DeckClickListener.kt
+++ app/src/main/java/com/example/KickOns/DeckClickListener.kt
@@ -9,7 +9,7 @@ package com.example.KickOns
 interface DeckClickListener {
 
     /**
-     * Onclick function that starts the game witht the selected deck
+     * Onclick function that starts the game with the selected deck
      *
      * @param deck The current view in the app
      * @see com.example.KickOns.MainActivity
