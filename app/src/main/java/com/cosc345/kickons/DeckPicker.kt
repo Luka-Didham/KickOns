4c9faf02b16e058075047f983e5a5fa29c7e8435
Kurt <wedku875@student.otago.ac.nz>
2022-10-08 18:33:32 +1300

    adding comments and cleaning up

--- app/src/main/java/com/cosc345/kickons/DeckPicker.kt
+++ app/src/main/java/com/cosc345/kickons/DeckPicker.kt
@@ -19,7 +19,12 @@ open class DeckPicker() : AppCompatActivity(), DeckClickListener {
     private lateinit var deckDao: DeckDAO
     private lateinit var deckSets: Array<MutableList<out Any>>
 
-
+    /**
+     * Function called upon creation of the DeckPicker Intent.
+     * Displays the DeckPicker.xml layout as the current activity,
+     * and readies the activity with all decks and cards.
+     *
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setOnline()
@@ -33,16 +38,18 @@ open class DeckPicker() : AppCompatActivity(), DeckClickListener {
         val btnHome = findViewById<Button>(R.id.btnOnline)
 
         //Speed at which items are deleted
-
         recyclerView.itemAnimator?.removeDuration = 5
         val swipeGesture = object : SwipeGesture(this){
+            /**
+             * Function to find if a deck entry has been swiped to be deleted,
+             * and deletes the swiped object.
+             */
             override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
                 when(direction){
                     ItemTouchHelper.LEFT ->{
                         delete(deckList[viewHolder.adapterPosition], viewHolder.adapterPosition)
                     }
                 }
-
             }
         }
 
@@ -59,17 +66,16 @@ open class DeckPicker() : AppCompatActivity(), DeckClickListener {
                     deckSets = response
                     deckList = response[1] as MutableList<DeckItem>
 
-                    GlobalScope.launch(Dispatchers.Main){binding.recyclerView.apply {
-                        layoutManager =
-                            LinearLayoutManager(applicationContext, LinearLayoutManager.VERTICAL, false)
-                        Log.d("dl", deckList.size.toString())
-                        adapter = DeckAdapter(deckList,mainActivity)
-                    }
+                    GlobalScope.launch(Dispatchers.Main){
+                        binding.recyclerView.apply {
+                            layoutManager =
+                                LinearLayoutManager(applicationContext, LinearLayoutManager.VERTICAL, false)
+                            Log.d("dl", deckList.size.toString())
+                            adapter = DeckAdapter(deckList,mainActivity)
+                        }
                     }
                 }
-
             })
-
         }
 
         btnHome.setOnClickListener{
@@ -78,11 +84,14 @@ open class DeckPicker() : AppCompatActivity(), DeckClickListener {
 
         btnCreateDeckFromChoose.setOnClickListener {
             val intent = Intent(this, DeckCreation::class.java)
-
             startActivity(intent)
         }
-
     }
+
+    /**
+     * Deletes a deck from the deck display screen, called by the onSwiped function.
+     *
+     */
     fun delete(deck: DeckItem, pos : Int){
         var position = deckSets[1].indexOf(deck)
         deckList.remove(deck)
@@ -93,11 +102,17 @@ open class DeckPicker() : AppCompatActivity(), DeckClickListener {
         }
     }
 
+    /**
+     * Defines where the home button will take you onClick.
+     */
     private fun home(){
         val intent = Intent(this,WelcomePage::class.java)
         startActivity(intent)
     }
 
+    /**
+     * Takes the user to the editing page for whichever deck they selected.
+     */
      override fun edit(deck: DeckItem) {
          val intent = Intent(this, EditDeck::class.java)
          intent.putExtra("id",deck.id)
@@ -109,13 +124,20 @@ open class DeckPicker() : AppCompatActivity(), DeckClickListener {
                     startActivity(intent)
                 }
             }
-
      }
 
+    /**
+     * Function to let the program know if the decks being viewed are local or online
+     * @see com.cosc345.kickons.DeckViewHolder
+     */
     open fun setOnline(){
         online = false
     }
 
+    /**
+     * Takes the activity to the main page, displaying the deck's cards
+     * @see com.cosc345.kickons.MainActivity
+     */
     override fun onClick(deck: DeckItem) {
        val intent = Intent(this, MainActivity::class.java)
         val pos = deckSets[1].indexOf(deck)
@@ -132,6 +154,9 @@ open class DeckPicker() : AppCompatActivity(), DeckClickListener {
         }
     }
 
+    /**
+     * Called to populate the cards to be shown in the MainActivity.kt activity.
+     */
     open suspend fun getCards(id: String) {
         cardList.clear()
         val cards = db.cardDAO().getByDeckId(id.toInt())
@@ -140,16 +165,25 @@ open class DeckPicker() : AppCompatActivity(), DeckClickListener {
         }
     }
 
+    /**
+     * Function called by the saveDeck function to save a deck's cards.
+     */
     fun saveCards(cardItem: CardItem){
         GlobalScope.launch{
             cardDao.addCard(cardItem)
         }
     }
+
+    /**
+     * A function to save a deck from the online database to local storage.
+     */
     suspend fun saveDeck(deck: DeckItem): Int {
         return deckDao.addDeck(deck).toInt()
-
     }
 
+    /**
+     * Queries the saved decks to populate the screen with the saved decks.
+     */
     open fun getDecks(myCallback: FirebaseCallback){
         deckList.clear()
         deckSets = emptyArray()
@@ -163,13 +197,12 @@ open class DeckPicker() : AppCompatActivity(), DeckClickListener {
         myCallback.onResponse(deckSets)
     }
 
-
-
+    /**
+     * Function to find a deck's ID in order to link up a deck title to its cards.
+     */
     open fun getDeckSetsID(deck: DeckItem):String {
         val pos = deckSets[1].indexOf(deck)
         val id = deckSets[0][pos]
         return id.toString()
     }
-
 }
-
